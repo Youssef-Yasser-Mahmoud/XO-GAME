ad5bfe967e567cfcdeb157f59a531a5b906f1ad5
Youssef <youssefyaser2020@gmail.com>
2025-08-17 14:07:24 +0300

    feat: update AppComponent template and structure; add Header, Card, and Footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
+import { CardComponent } from "./components/card/card.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, CardComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
