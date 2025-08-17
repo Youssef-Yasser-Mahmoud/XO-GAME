ee721345276399628169601f12481b57cb31dd7e
Youssef <youssefyaser2020@gmail.com>
2025-08-17 14:07:58 +0300

    refactor: remove unused styleUrl property from Card, Header, and Footer components

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-card',
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css',
 })
 export class CardComponent {}
